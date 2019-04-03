76341ebf098b2f1ea45ed7c286fc71077b5e695b
Szücsi <hun46y3@hotmail.hu>
2019-04-03 17:32:20 +0200

    some improvements

--- week-01/day-3/7-PlusOne/main.cpp
+++ week-01/day-3/7-PlusOne/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 
 //  Create a function that takes two strings as a parameter
 //  Returns the starting index where the second one is starting in the first one
 //  Returns `-1` if the second string is not in the first one
 
-int substr(std::string str, std::string keyword) {
+int substr1(std::string str, std::string keyword) {
     int poz = 0;
     poz = str.find(keyword);
     if (poz==0) return -1;
     else return poz;
+}
+
+int substr2(std::string str, std::string keyword) {
+    for (int i = 0; i < keyword.length(); ++i) {
+        for (int j = 0; j < str.length(); ++j) {
+            if (keyword[i] == str[j]){
+                
+            }
+        }
+        
+    }
 }
 
 
@@ -19,10 +30,16 @@ int main() {
     // Example
 
     // Should print: '17'
-    std::cout << substr("this is what I'm searching in", "searching") << std::endl;
+    std::cout << substr1("this is what I'm searching in", "searching") << std::endl;
 
     // Should print: '-1'
-    std::cout << substr("this is what I'm searching in", "not") << std::endl;
+    std::cout << substr1("this is what I'm searching in", "not") << std::endl;
+
+    // Should print: '17'
+    std::cout << substr2("this is what I'm searching in", "searching") << std::endl;
+
+    // Should print: '-1'
+    std::cout << substr2("this is what I'm searching in", "not") << std::endl;
 
     return 0;
 }
