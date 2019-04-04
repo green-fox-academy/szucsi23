111e17fb0293a2b173d0326e7bdb7cf11fc912ac
Szücsi <hun46y3@hotmail.hu>
2019-04-04 12:47:18 +0200

    arrays 2.5

--- week-01/day-4/2-Arrays-p2/main.cpp
+++ week-01/day-4/2-Arrays-p2/main.cpp
@@ -64,6 +64,15 @@ int main() {
     abc[0] = abc[2];
     abc[2] = temp;
 
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+    int ai[] = {3, 4, 5, 6, 7};
+    int sum = 0;
+    for (int i = 0; i < 5; ++i) {
+        sum += ai[i];
+    }
+    std::cout << sum << std::endl;
     
 
 
