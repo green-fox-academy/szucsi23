c038b5740a78c2664e0d994e80cff9608f7f716a
Szücsi <hun46y3@hotmail.hu>
2019-04-04 09:27:39 +0200

    2. task

--- week-01/day-4/1-Functions/main.cpp
+++ week-01/day-4/1-Functions/main.cpp
@@ -2,21 +2,33 @@
 
 int doubling (int);
 
+void greet(std::string);
+
 int main() {
 
     // - Create an integer variable named `baseNum` and assign the value `123` to it
     // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
     // - Print the result of `doubling(baseNum)`
     int baseNum = 123;
-    doubling(baseNum);
+    baseNum = doubling(baseNum);
     std::cout << baseNum << std::endl;
 
-    
+    // - Create a string variable named `al` and assign the value `Greenfox` to it
+    // - Create a function called `greet` that greets it's input parameter
+    // - Greeting is printing e.g. `Greetings dear, Greenfox`
+    // - Greet `al`
+    std::string al = "Greenfox";
+    greet(al);
+
 
 
     return 0;
 }
 
 int doubling (int a) {
-    return a*2;
+    return (a*2);
+}
+
+void greet (std::string a) {
+    std::cout << "Welcome, dear "  << a << "!" << std::endl;
 }
