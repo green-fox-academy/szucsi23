9fc78a6561246bdda7ea1c4adbed1f2f7af737d2
Szücsi <hun46y3@hotmail.hu>
2019-04-04 09:58:35 +0200

    arrays 2.0

--- week-01/day-4/2-Arrays-p1/main.cpp
+++ week-01/day-4/2-Arrays-p1/main.cpp
@@ -24,6 +24,30 @@ int main() {
     int r[] = {54, 23, 66, 12};
     std::cout << r[1]+r[2] << std::endl;
 
+    // - Create an array variable named `s`
+    //   with the following content: `[1, 2, 3, 8, 5, 6]`
+    // - Change the 8 to 4
+    // - Print the fourth element
+    int s[] = {1, 2, 3, 8, 5, 6};
+    s[3] = 4;
+    std::cout << s[3] << std::endl;
 
+    // - Create an array variable named `t`
+    //    with the following content: `[1, 2, 3, 4, 5]`
+    // - Increment the third element
+    // - Print the third element
+    int t[] = {1, 2, 3, 4, 5};
+    t[2]++;
+    std::cout << s[2] << std::endl;
+
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `numbers`
+    int numbers[] = {4, 5, 6, 7};
+    for (int i = 0; i < (sizeof(numbers)/ sizeof(int)); ++i) {
+        std::cout << numbers[i] << std::endl;
+    }
+
+    
     return 0;
 }
