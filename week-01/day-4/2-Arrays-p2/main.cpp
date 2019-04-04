512f8780b67d1417bffd98a07205e488a181a430
Szücsi <hun46y3@hotmail.hu>
2019-04-04 12:38:35 +0200

    arrays 2.3

--- week-01/day-4/2-Arrays-p2/main.cpp
+++ week-01/day-4/2-Arrays-p2/main.cpp
@@ -48,8 +48,15 @@ int main() {
             {"orchid", "violet", "pink", "hot pink"}
     };
 
-    
+    // - Create an array variable named `animals`
+    //   with the following content: `["koal", "pand", "zebr"]`
+    // - Add all elements an `"a"` at the end
+        std::string animals[] = {"koal", "pand", "zebr"};
+    for (int i = 0; i < 3; ++i) {
+        animals[i] += "a";
+    }
 
+    
 
 
     return 0;
