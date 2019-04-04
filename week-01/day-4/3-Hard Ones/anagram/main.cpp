99557afda3eae9c3bdac6ba68175dfa266d44285
Szücsi <hun46y3@hotmail.hu>
2019-04-04 15:18:44 +0200

    working on hard ones, almost giving up on anagram as well

--- week-01/day-4/3-Hard Ones/anagram/main.cpp
+++ week-01/day-4/3-Hard Ones/anagram/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
+bool anagram (std::string, std::string);
+
 int main() {
-    
+
+    std::string word1;
+    std::string word2;
+
+    std::cout << "Give me two words." << std::endl;
+    std::cin >> word1 >> word2;
+
+    bool namivan = anagram(word1, word2);
+
+    if (namivan) std::cout << "These two are anagrams." << std::endl;
+    else std::cout << "These are not anagrams of eachoder." << std::endl;
+
     return 0;
+}
+
+bool anagram (std::string in1, std::string in2) {
+
+    int size = in1.length();
+    bool yup = true;
+
+
+    bool Checked[size];
+    for (int i = 0; i < size; i++) Checked[i] = false;
+
+    for (int i = 0; i < size; i++) {
+        if (Checked[i] == false) {
+            for (int j = 0; j < size; ++j) {
+                if (in1[i] == in2[j]) {
+
+                    Checked[i] = true;
+                }
+            }
+
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        if (Checked[i] == false) yup = false;
+    }
+    return yup;
 }
