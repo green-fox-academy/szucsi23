0f6aa300ca98e28d268d7e97a709c9e799fa15ce
Szücsi <hun46y3@hotmail.hu>
2019-04-08 12:27:20 +0200

    2. done

--- week-02/day-1/Pointers-1/main.cpp
+++ week-02/day-1/Pointers-1/main.cpp
@@ -18,6 +18,15 @@ int main() {
 
     //------------------------------------------------------------------------------------------------------------------
 
+    // Print out the value of the variable called 'age' using a pointer
+    // (you have to create the pointer first)
+
+    int age = 31;
+    int *ageptr = &age;
+    std::cout << *age << std::endl;
+
+    //------------------------------------------------------------------------------------------------------------------
+
     
 
 
