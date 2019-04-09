2b2cca948dbd31cbb6505805bd08af3897b26158
Szücsi <hun46y3@hotmail.hu>
2019-04-09 11:45:39 +0200

    still working on strings

--- week-02/day-2/Strings/main.cpp
+++ week-02/day-2/Strings/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+std::string reverse(std::string str);
+
 int main() {
 
     std::string example("In a dishwasher far far away");
@@ -43,7 +45,28 @@ int main() {
 
     //------------------------------------------------------------------------------------------------------------------
 
-    
+    std::string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    // Create a method that can reverse an std:string, which is passed as the parameter
+    // Use it on this reversed string to check it!
+    // Try to solve this using .at() first, and optionally anything else after.
+    // Hint: You might use a temporary variable to swap 2 characters or you can use std::swap function.
+
+    std::cout << reverse(reversed) << std::endl;
+
+    //------------------------------------------------------------------------------------------------------------------
 
     return 0;
+}
+
+std::string reverse(std::string str) {
+    int size = str.length();
+    std::string swapped = "";
+
+    for (int i = 0; i < size; ++i) {
+        swapped += str.at(size-i-1);
+
+    }
+
+    return swapped;
 }
