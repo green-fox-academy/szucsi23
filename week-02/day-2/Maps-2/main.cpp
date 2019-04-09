b8d6c69a8bf17c100d04099b24b28c3ab7093b8f
Szücsi <hun46y3@hotmail.hu>
2019-04-09 17:28:05 +0200

    working even some more

--- week-02/day-2/Maps-2/main.cpp
+++ week-02/day-2/Maps-2/main.cpp
@@ -27,11 +27,91 @@ int main() {
     //Do we know Chris E. Myers' phone number?
     //no.
 
-    if(phonebook.end() == phonebook.find("Chris E. Myers")) {
+    if (phonebook.end() == phonebook.find("Chris E. Myers")) {
         std::cout << "nope" << std::endl;
     }
 
+    //------------------------------------------------------------------------------------------------------------------
 
+    std::map<std::string, int> products;
+    products = {
+            {"Eggs",    200},
+            {"Milk",    200},
+            {"Fish",    400},
+            {"Apples",  150},
+            {"Bread",   50},
+            {"Chicken", 550}
+    };
+
+    std::map<std::string, int>::iterator iter;
+
+    // How much is the fish?
+    std::cout << std::endl;
+    iter = products.find("Fish");
+    std::cout << iter->second << std::endl;
+
+    // What is the most expensive product?
+    std::cout << std::endl;
+    int max = iter->second;
+    int sum = 0;
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second > max) {
+            max = iter->second;
+        }
+        sum += iter->second;
+    }
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second == max) {
+            std::cout << iter->first << std::endl;
+        }
+    }
+    // What is the average price?
+    std::cout << std::endl;
+    int avg = sum/products.size();
+    // How many products' price is below 300?
+    std::cout << std::endl;
+    int below = 0;
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second < 300) {
+            below++;
+        }
+    }
+    // Is there anything we can buy for exactly 125?
+    std::cout << std::endl;
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second == 125) {
+            std::cout << iter->first << std::endl;
+        }
+    }
+    // What is the cheapest product?
+    std::cout << std::endl;
+    iter = products.begin();
+    int min = iter->second;
+
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second < min) {
+            min = iter->second;
+        }
+    }
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second == min) {
+            std::cout << iter->first << std::endl;
+        }
+    }
+    //Which products cost less than 201? (just the name)
+    std::cout << std::endl;
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second < 201) {
+            std::cout << iter->first << std::endl;
+        }
+    }
+    //Which products cost more than 150? (name + price)
+    std::cout << std::endl;
+    for (iter = products.begin(); iter != products.end(); iter++) {
+        if (iter->second > 150) {
+            std::cout << iter->first << " " << iter->second<< std::endl;
+        }
+    }
 
     return 0;
 }
