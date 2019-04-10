76e39e0724141e5e0d02ff1c65cfc0589d60251b
Szücsi <hun46y3@hotmail.hu>
2019-04-10 14:42:15 +0200

    positioning done

--- week-02/day-3/Positioning/main.cpp
+++ week-02/day-3/Positioning/main.cpp
@@ -1,7 +1,3 @@
-// Exercise:
-// draw four different size and color rectangles.
-// avoid code duplication.
-
 #include <iostream>
 #include <SDL.h>
 
@@ -9,61 +5,93 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+//Draws geometry on the canvas
+void draw();
+
 //Starts up SDL and creates window
 bool init();
 
 //Frees media and shuts down SDL
 void close();
 
-void draw();
+void sqrdraw(int, int);
 
 //The window we'll be rendering to
-SDL_Window *gWindow = nullptr;
+SDL_Window* gWindow = nullptr;
 
 //The window renderer
-SDL_Renderer *gRenderer = nullptr;
+SDL_Renderer* gRenderer = nullptr;
 
-bool init() {
+void draw()
+{
+    // create a square drawing function that takes 2 parameters:
+    // the x and y coordinates of the square's top left corner
+    // and draws a 50x50 square from that point.
+    // draw at least 3 squares with that function.
+    // avoid code duplication.
+    int x = 20;
+    int y = 100;
+    for (int i = 0; i < 3; ++i) {
+        sqrdraw(x*(i+1), y*(i+1));
+    }
+
+}
+
+void sqrdraw(int x, int y)
+{
+    SDL_SetRenderDrawColor(gRenderer, 10, 200, 130, 0xFF);
+    SDL_Rect rect = {x, y, 50, 50};
+    SDL_RenderFillRect(gRenderer, &rect);
+
+}
+
+bool init()
+{
     //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
         std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Create window
-    gWindow = SDL_CreateWindow("Four rectangles", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (gWindow == nullptr) {
+    gWindow = SDL_CreateWindow( "Position square", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( gWindow == nullptr )
+    {
         std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Create renderer for window
-    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-    if (gRenderer == nullptr) {
+    gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+    if( gRenderer == nullptr )
+    {
         std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Initialize renderer color
-    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
     return true;
 }
 
-void close() {
+void close()
+{
     //Destroy window
-    SDL_DestroyRenderer(gRenderer);
-    SDL_DestroyWindow(gWindow);
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
     gWindow = nullptr;
     gRenderer = nullptr;
 
     SDL_Quit();
 }
 
-int main(int argc, char *args[]) {
+int main( int argc, char* args[] )
+{
     //Start up SDL and create window
-    if (!init()) {
+    if( !init() )
+    {
         std::cout << "Failed to initialize!" << std::endl;
         close();
         return -1;
@@ -76,7 +104,7 @@ int main(int argc, char *args[]) {
     SDL_Event e;
 
     //While application is running
-    while (!quit) {
+    while( !quit ) {
         //Handle events on queue
         while (SDL_PollEvent(&e) != 0) {
             //User requests quit
@@ -89,10 +117,7 @@ int main(int argc, char *args[]) {
         SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(gRenderer);
 
-        //--------------------------------------------
-        //This is where you can start drawing geometry
         draw();
-        //--------------------------------------------
 
         //Update screen
         SDL_RenderPresent(gRenderer);
@@ -102,14 +127,4 @@ int main(int argc, char *args[]) {
     close();
 
     return 0;
-}
-
-void draw() {
-    for (int i = 0; i < 4; ++i) {
-
-        SDL_SetRenderDrawColor(gRenderer, 10*i, 200*i, 3000000*i, 0xFF);
-        SDL_Rect rect = {10 + 100 * i, 10 + 100 * i, 10 + 10 * i, 10 + 10 * i};
-        SDL_RenderFillRect(gRenderer, &rect);
-
-    }
 }
