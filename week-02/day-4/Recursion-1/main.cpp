477bb2e93c3ceea141887b3ac94b95febf01aba2
Szücsi <hun46y3@hotmail.hu>
2019-04-11 12:02:40 +0200

    Working on recursions, 4 done

--- week-02/day-4/Recursion-1/main.cpp
+++ week-02/day-4/Recursion-1/main.cpp
@@ -3,27 +3,37 @@
 int adder(int);
 int digits(int);
 int power(int, int);
+int lnko(int, int);
 
 int main()
 {
+    //setup
     std::cout << "Give me a number!" << std::endl;
     int x = 0;
     std::cin >> x;
-    int sum = 0;
+    int result = 0;
 
-    sum = adder(x);
-    std::cout << "Sum of previous numbers: " << sum << std::endl;
+    //sum
+    result = adder(x);
+    std::cout << "Sum of previous numbers: " << result << std::endl;
 
+    //digits sum
+    result = digits(x);
+    std::cout << "Sum of digits: " << result << std::endl;
 
-    sum = digits(x);
-    std::cout << "Sum of digits: " << sum << std::endl;
-
+    //2. setup
     std::cout << "Give me two numbers!" << std::endl;
     int y = 0;
     std::cin >> x >> y;
 
-    sum = power(x, y);
-    std::cout << x << " to the " << y << "'th power is " << sum << std::endl;
+    //power
+    result = power(x, y);
+    std::cout << x << " to the " << y << "'th power is " << result << std::endl;
+
+    //GCD
+    result = lnko(x, y);
+    std::cout << "The greatest common divisor of "<< x << " and " << y << " is " << result << "." <<std::endl;
+
 
 
 
@@ -53,4 +63,17 @@ int power(int x, int y)
         return x;
     }
     return (x = (x * power(x, y-1)));
+}
+
+int lnko(int x, int y)
+{
+    if(x < y) {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+    if(x%y==0) {
+        return y;
+    }
+    return lnko(y, x%y);
 }
