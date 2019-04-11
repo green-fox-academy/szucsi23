6088763dcfcd8b6e3826e2a9936d63428d7cdee2
Szücsi <hun46y3@hotmail.hu>
2019-04-11 11:51:02 +0200

    Working on recursions, 3 done

--- week-02/day-4/Recursion-1/main.cpp
+++ week-02/day-4/Recursion-1/main.cpp
@@ -2,9 +2,11 @@
 
 int adder(int);
 int digits(int);
+int power(int, int);
 
 int main()
 {
+    std::cout << "Give me a number!" << std::endl;
     int x = 0;
     std::cin >> x;
     int sum = 0;
@@ -16,6 +18,12 @@ int main()
     sum = digits(x);
     std::cout << "Sum of digits: " << sum << std::endl;
 
+    std::cout << "Give me two numbers!" << std::endl;
+    int y = 0;
+    std::cin >> x >> y;
+
+    sum = power(x, y);
+    std::cout << x << " to the " << y << "'th power is " << sum << std::endl;
 
 
 
@@ -33,8 +41,16 @@ int adder(int x)
 
 int digits(int x)
 {
-    if ( x <= 1) {
+    if ( x / 10 == 0) {
         return x;
     }
     return ((x%10)+digits(x/10));
+}
+
+int power(int x, int y)
+{
+    if(y==1){
+        return x;
+    }
+    return (x = (x * power(x, y-1)));
 }
