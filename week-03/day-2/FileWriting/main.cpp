aa34df52b09aa7a57b96fd0b4154a531b5d774bd
Szücsi <hun46y3@hotmail.hu>
2019-04-16 13:03:40 +0200

    FileWriting done

--- week-03/day-2/FileWriting/main.cpp
+++ week-03/day-2/FileWriting/main.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <fstream>
 
+void funckit (std::string, std::string, int);
+
+
 int main()
 {
     // Open a file called "my-file.txt"
@@ -21,6 +24,29 @@ int main()
         std::cout << e.what() << std::endl;
     }
 
+    //------------------------------------------------------------------------------------------------------------------
 
+    // Create a function that takes 3 parameters: a path, a word and a number
+    // and is able to write into a file.
+    // The path parameter should be a string that describes the location of the file you wish to modify
+    // The word parameter should also be a string that will be written to the file as individual lines
+    // The number parameter should describe how many lines the file should have.
+    // If the word is "apple" and the number is 5, it should write 5 lines
+    // into the file and each line should read "apple"
+
+    std::string path = "C:\\Users\\hun46\\Documents\\Programming\\GreenFox\\szucsi23\\week-03\\day-2\\FileWriting\\alma.txt";
+    std::string word = "alma";
+    int num = 3;
+
+    funckit(path, word, num);
     return 0;
+}
+
+void funckit(std::string path, std::string word, int num)
+{
+    std::ofstream file;
+    file.open(path);
+    for (int i = 0; i < num; ++i) {
+        file << word << std::endl;
+    }
 }
