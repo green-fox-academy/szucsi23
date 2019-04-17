e530a585c56b6311396b11cfafd4f3dca185c3db
Szücsi <hun46y3@hotmail.hu>
2019-04-17 14:09:34 +0200

    Fleet of Things done

--- week-03/day-3/FleetOfThings/main.cpp
+++ week-03/day-3/FleetOfThings/main.cpp
@@ -6,10 +6,24 @@ int main(int argc, char* args[])
 {
     Fleet fleet;
     // Create a fleet of things to have this output:
+
     // 1. [ ] Get milk
+    Thing milk("Get milk");
+    fleet.add(milk);
+
     // 2. [ ] Remove the obstacles
+    Thing obstacles("Remove the obstacles");
+    fleet.add(obstacles);
+
     // 3. [x] Stand up
+    Thing stand("Stand up");
+    stand.complete();
+    fleet.add(stand);
+
     // 4. [x] Eat lunch
+    Thing eat("Eat lunch");
+    eat.complete();
+    fleet.add(eat);
 
     std::cout << fleet.toString() << std::endl;
     return 0;
