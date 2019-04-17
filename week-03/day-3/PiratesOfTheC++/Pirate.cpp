2cf3e7a9de4c5ebc17db988f2bce15e96880badd
Szücsi <hun46y3@hotmail.hu>
2019-04-17 17:30:38 +0200

    i should leave something for tomorrow

---
+++ week-03/day-3/PiratesOfTheC++/Pirate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hun46 on 2019. 04. 17..
+//
+
+#include "Pirate.h"
---
+++ week-03/day-3/PiratesOfTheC++/Pirate.h
@@ -0,0 +1,19 @@
+//
+// Created by hun46 on 2019. 04. 17..
+//
+
+#ifndef PIRATESOFTHEC___PIRATE_H
+#define PIRATESOFTHEC___PIRATE_H
+
+
+class Pirate {
+public:
+private:
+    int drunkness;
+    int hp;
+    bool parrot;
+
+};
+
+
+#endif //PIRATESOFTHEC___PIRATE_H
