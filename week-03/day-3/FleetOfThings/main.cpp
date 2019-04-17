404386980885a0543abe67d71e8201359b6926b0
Szücsi <hun46y3@hotmail.hu>
2019-04-17 14:00:07 +0200

    Pokemon done

---
+++ week-03/day-3/FleetOfThings/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "fleet.h"
+
+int main(int argc, char* args[])
+{
+    Fleet fleet;
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+
+    std::cout << fleet.toString() << std::endl;
+    return 0;
+}
