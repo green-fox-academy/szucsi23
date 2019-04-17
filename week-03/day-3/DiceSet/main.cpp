f4c667d3fe0c88be0d400e45466f6a8d8feb1699
Szücsi <hun46y3@hotmail.hu>
2019-04-17 14:34:08 +0200

    Dices done

--- week-03/day-3/DiceSet/main.cpp
+++ week-03/day-3/DiceSet/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <ctime>
 
-int main()
+#include "dice_set.h"
+
+int main(int argc, char* args[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    // You have a `DiceSet` class which has a list for 6 dices
+    // You can roll all of them with roll()
+    // Check the current rolled numbers with getCurrent()
+    // You can reroll with roll()
+    // Your task is to roll the dices until all of the dices are 6
+    std::clock_t start;
+    double duration;
+
+    DiceSet diceSet;
+    int sixes = 0;
+
+    diceSet.roll();
+    for (int i = 0; i < 6; ++i) {
+        std::cout << diceSet.getCurrent(i) << " ";
+    }
+    std::cout << std::endl;
+
+    start = std::clock();
+    while(sixes != 6) {
+        sixes = 0;
+        diceSet.roll();
+
+        for (int i = 0; i < 6; ++i) {
+            if(diceSet.getCurrent(i) == 6) sixes++;
+        }
+    }
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    std::cout << duration << std::endl;
+    for (int i = 0; i < 6; ++i) {
+        std::cout << diceSet.getCurrent(i) << " ";
+    }
+
     return 0;
 }
