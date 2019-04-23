2531c8829ccecdcf3f14e38fb6579d8f20e06163
Szücsi <hun46y3@hotmail.hu>
2019-04-23 13:26:10 +0200

    Sponsor class done

---
+++ week-04/day-2/GreenFoxOrganization/Sponsor.cpp
@@ -0,0 +1,34 @@
+//
+// Created by hun46 on 2019. 04. 23..
+//
+
+#include "Sponsor.h"
+
+Sponsor::Sponsor() : Person()
+{
+    _company = "Google";
+    _hiredStudents = 0;
+}
+
+Sponsor::Sponsor(std::string name, int age, Gender gender, std::string company) : Person(name, age, gender)
+{
+    _company = company;
+    _hiredStudents = 0;
+}
+
+void Sponsor::introduce()
+{
+    std::cout << "Hello, my name is " << getName() << " and I'm a " << getAge() << " years old " << getRealGender();
+    std::cout << " who represents " << _company << " and hired " << _hiredStudents << " students so far.";
+}
+
+void Sponsor::getGoal()
+{
+    std::cout << "My goal is: Hire brilliant junior software developers." << std::endl;
+}
+
+void Sponsor::hire()
+{
+    _hiredStudents++;
+}
+
