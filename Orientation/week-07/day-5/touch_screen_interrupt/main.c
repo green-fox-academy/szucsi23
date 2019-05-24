7b17901ee70a568301b3be397aa3aac899f170a3
Szücsi <hun46y3@hotmail.hu>
2019-05-24 17:32:55 +0200

    added hidden timer to countdown

--- Orientation/week-07/day-5/touch_screen_interrupt/main.c
+++ Orientation/week-07/day-5/touch_screen_interrupt/main.c
@@ -13,44 +13,48 @@ int setup();
 int main(void)
 {
 	init_all();
-
 	TS_StateTypeDef ts_state;
-
-
-	char clock_buffer[3] = {0,0,0};
+	char clock_buffer[49];
 
 	while (1) {
 
 		int clock = setup();
-		BSP_LCD_DisplayStringAt(79, 10, "Touch the screen, when the timer reaches Zero!", LEFT_MODE);
+		int no_show = (HAL_RNG_GetRandomNumber(&rng_handle) % (clock / 2)) + 2;
+		int miliseconds = clock * 1000;
 
-		for (; clock > 0; clock--) {
+		for (; clock >= 0; clock--) {
+			if (clock > no_show) {
+				sprintf(clock_buffer,"Touch the screen, when the timer reaches Zero: %d",clock);
+				BSP_LCD_DisplayStringAt(68, 10, clock_buffer, LEFT_MODE);
 
+				HAL_Delay(1000);
+				BSP_LCD_Clear(0x00000000);
+			} else {
+				BSP_LCD_DisplayStringAt(68, 10, "Touch the screen, when the timer reaches Zero: ?", LEFT_MODE);
+
+				HAL_Delay(1000);
+			}
 		}
 
 
-		BSP_TS_GetState(&ts_state);
-		if (ts_state.touchDetected) {
-			counter_changed = 0;
-			//BSP_LCD_Clear(0x00000000);
-			BSP_LCD_DrawCircle(ts_state.touchX[0], ts_state.touchY[0], 50);
-
+		while (reset != 1) {
 		}
-		while(reset != 1){}
 	}
 }
 
 void init_all()
-{;
+{
+	;
 	HAL_Init();
-		SystemClock_Config();
-		__HAL_RCC_RNG_CLK_ENABLE();
+	SystemClock_Config();
+	__HAL_RCC_RNG_CLK_ENABLE()
+	;
 
-		LCD_init();
-		TS_init();
-		PB_init();
-		rng_handle.Instance = RNG;
-		HAL_RNG_Init(&rng_handle);
+	LCD_init();
+	TS_init();
+	PB_init();
+	rng_handle.Instance = RNG;
+	HAL_RNG_Init(&rng_handle);
 
 }
 
@@ -63,8 +67,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 int setup()
 {
 	reset = 0;
-	int wait = (HAL_RNG_GetRandomNumber(&rng_handle) % 25)+5;
+	int wait = (HAL_RNG_GetRandomNumber(&rng_handle) % 25) + 5;
 	return wait;
 }
-
 
